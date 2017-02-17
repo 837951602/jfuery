e87fc2ec948cd5c2b88763f8786381de3ddfac5e
837951602 <837951602@qq.com>
2017-02-17 12:00:32 +0800

    Create generator.js
    This file is a JScript file. Don't run it under node.js or any strange environment

---
+++ generator.js
@@ -0,0 +1,37 @@
+var fso  = new ActiveXObject ("Scripting.FileSystemObject");
+var base = fso.OpenTextFile  ("base.txt", 1);
+var code = fso.OpenTextFile  ("code.txt", 1);
+var out  = fso.CreateTextFile("jfuery.js");
+out.Write ('[');
+out.Write (base.readLine());
+var db   = code.readAll();
+//WScript.Echo (db);
+function w(v) {
+  out.Write ([
+    '[[]]()',  '()[[]]',  '()()',    '([])',
+    '(![])',   '(+[])',   '([[]])',  '(!![])',
+    '()([])',  '([![]])', '([+[]])', '([]+[])',
+    '(+!![])', '[![]]()', '[+[]]()', '()[+[]]'
+  ][v&15]);
+}
+for (var i=0; i<db.length; i++) {
+  if (i%50 == 0) {
+    if (i) out.Write ('()');
+    out.Write ('+[][[]]');
+  }
+  var s = db.charCodeAt(i);
+  if (s<256) {
+    w (s >> 4);
+    w (s);
+  } else {
+    out.Write ('()[![]]');
+    w (s >> 12);
+    w (s >> 8);
+    w (s >> 4);
+    w (s);
+  }
+}
+out.Write ('()][[]]()[!![]]()');
+base.Close();
+code.Close();
+out.Close();
